29b56c2487b8fcc6908ed54c9e64188ba979e3f4
LohurLaura <lohurlaura@gmail.com>
2023-09-25 23:53:07 +0300

    Initial commit

--- src/main/java/Asterisks.java
+++ src/main/java/Asterisks.java
@@ -2,6 +2,7 @@ public class Asterisks {
     public static void main(String[] args) {
         System.out.println("*  *");
         System.out.println("***");
+        System.out.println("* *");
 
 
     }
