5391ea3816a7d07246c865d68aa4dd6aa149df71
LohurLaura <lohurlaura@gmail.com>
2023-09-25 23:51:45 +0300

    Initial commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,9 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("White");
+        System.out.println("Black");
+        System.out.println("Green");
+    }
+}
